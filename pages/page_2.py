b664b1fc3680d4249bb3e9998992ba130a94a17e
Tannmay Khandelwal <94522751+tannmaycoding@users.noreply.github.com>
2024-09-13 18:38:58 +0530

    Update page_2.py

--- pages/page_2.py
+++ pages/page_2.py
@@ -1,25 +1,27 @@
 import streamlit as st
 import pandas as pd
+import sqlalchemy
 
 st.title("Sign Up")
 username = st.text_input("Username: ")
 emailId = st.text_input("Email address: ")
 password = st.text_input("Password: ")
 splChars = ["'", "=", "+", "-", "*", "/", "^", "@", "#", "$", "%", "&", "(", ")"]
+engine = sqlalchemy.create_engine("mysql+pymysql://root:monadarling123@localhost:3306/accounts")
 
 if st.button("Sign Up"):
     df_new = pd.DataFrame({"username": [username], "email": [emailId], "password": [password]})
 
     try:
         # Read the existing data from the Excel file
-        df_existing = pd.read_excel("ids.xlsx")
+        df_existing = pd.read_sql("share-market-login", con=engine)
     except FileNotFoundError:
         # If the file doesn't exist, create an empty DataFrame
         df_existing = pd.DataFrame(columns=["username", "email", "password"])
 
     # Validation checks
-    if "=" in username:
-        st.error("No = sign allowed")
+    if "`" in username:
+        st.error("No ` sign allowed")
         st.stop()
 
     elif username in df_existing.username.values:
@@ -35,7 +37,7 @@ if st.button("Sign Up"):
         df_combined = pd.concat([df_existing, df_new], ignore_index=True)
 
         # Save the updated DataFrame back to the Excel file
-        df_combined.to_excel("ids.xlsx", index=False)
+        df_combined.to_sql("share-market-login", con=engine, if_exists="append", index=False)
 
         # Successful registration, switch to another page
         st.success("Sign Up Successful!")
